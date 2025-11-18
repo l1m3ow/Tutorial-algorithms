7093410159db16f6f65828b62e061bc77b851fa5
l1m3ow <retro.dilop.06@gmail.com>
2025-11-18 19:25:28 +0300

    2

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var n int             // объявляем переменную n размер массива
-	fmt.Scan(&n)          // передаём значение n через консоль
-	arr := make([]int, n) // создаём массив размером n
+	var n, per, k int
+	fmt.Print("Введите размер массива n: ")
+	fmt.Scan(&n)
+	fmt.Print("Введите значение элемента, которое ищем per: ")
+	fmt.Scan(&per)
+	arr := make([]int, n)
 
-	for i := 1; i <= n; i++ {
-		arr[i-1] = i
+	for i := 0; i < n; i++ {
+		arr[i] = i + 1
 	}
 
-	fmt.Print(arr)
+	left, right := 0, n-1
+	k = 0
+
+	for left <= right {
+		k++
+		mid := (left + right) / 2
+
+		if arr[mid] == per {
+			fmt.Printf("Количество проверок k = %d \n", k)
+			return
+		} else if arr[mid] < per {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
 }
