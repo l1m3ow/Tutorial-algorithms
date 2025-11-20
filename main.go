a48fbf8814692e61c9189d1eedd0ce49dcdde58e
l1m3ow <retro.dilop.06@gmail.com>
2025-11-20 19:36:52 +0300

    make 2 Tems

--- main.go
+++ main.go
@@ -6,24 +6,23 @@ func main() {
 	var n int
 	fmt.Print("Введите до какого числа N ищете простые числа: ")
 	fmt.Scan(&n)
-	prime := make([]bool, n+1)
+	size := (n - 1) / 2
+	prime := make([]bool, size+1)
 
-	for i := 2; i <= n; i++ {
+	for i := 0; i <= size; i++ {
 		prime[i] = true
 	}
 
-	for i := 2; i*i <= n; i++ {
-		if prime[i] {
-			for j := i * i; j <= n; j += i {
-				prime[j] = false
-			}
+	for i := 1; i <= size; i++ {
+		for j := 1; i+j+2*i*j <= size; j++ {
+			prime[i+j+2*i*j] = false
 		}
 	}
 
-	fmt.Print("Простые числа: ")
-	for i := 2; i <= n; i++ {
+	fmt.Print("Простые числа: 2 ")
+	for i := 1; i <= size; i++ {
 		if prime[i] {
-			fmt.Print(i, " ")
+			fmt.Print(i*2+1, " ")
 		}
 	}
 }
