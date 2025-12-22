8e5d837b1811cb5e853ea6a02f69ffa19b5e0ba2
l1m3ow <retro.dilop.06@gmail.com>
2025-12-22 07:35:26 +0300

    rec

--- main.go
+++ main.go
@@ -1,37 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func Gnome(arr []int) {
-	start := time.Now()
-
-	i := 1
-
-	for i < len(arr) {
-		if i > 0 && arr[i-1] > arr[i] {
-			arr[i], arr[i-1] = arr[i-1], arr[i]
-			i--
-		} else {
-			i++
-		}
+func Sum(n int) int {
+	if n < 10 {
+		return n
 	}
-	elapsed := time.Since(start)
-	fmt.Printf("Время в наносекундах: %d ns\n", elapsed.Nanoseconds())
+	return n%10 + Sum(n/10)
 }
 
 func main() {
-	var size int
-	fmt.Print("Введите размер массива: ")
-	fmt.Scan(&size)
-
-	arr := make([]int, size)
-	for i := 0; i < size; i++ {
-		arr[i] = rand.Intn(101)
-	}
-
-	Gnome(arr)
+	var n int
+	fmt.Print("Введите число n: ")
+	fmt.Scan(&n)
+	ot := Sum(n)
+	fmt.Printf("Сумма чисел числа %d = %d", n, ot)
 }
